2a9ffd1a9d2e55c0e9d11afd8cee4bf6e0e8e904
Carlosnoe10 <carlosdnoem@gmail.com>
2023-02-10 12:12:05 -0800

    Main avanzado

--- src/lab4p2_/equipo10/Lab4P2_Equipo10.java
+++ src/lab4p2_/equipo10/Lab4P2_Equipo10.java
@@ -1,13 +1,22 @@
 
 package lab4p2_.equipo10;
 
+import java.util.Scanner;
+
 public class Lab4P2_Equipo10 {
 
-    static String [][] matrix = new String [8][8];
-    
+    static Object [][] matrix = new Object [8][8];
+    Scanner k1ng =new Scanner(System.in);
     public static void main(String[] args) {
+        Scanner k1ng =new Scanner(System.in);
         String comando = "";
-        System.out.println("");
+        System.out.println("Ingrese su nombre Jugador de piezas blancas ");
+        String Blancas = k1ng.nextLine();
+//        for (int i = 0; i < 9; i++) {
+//            [8][i]
+//        }
+//        Peon c= new Peon(); 
+        
         do{
             
         }while(comando!="gusbai");
