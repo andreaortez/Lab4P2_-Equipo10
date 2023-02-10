eaf74f161185350168794b64f8337d7702d3a1ac
Andrea Ortez <andreaortez11@unitec.edu>
2023-02-10 13:35:55 -0600

    Inicio

---
+++ src/lab4p2_/equipo10/Lab4P2_Equipo10.java
@@ -0,0 +1,16 @@
+
+package lab4p2_.equipo10;
+
+public class Lab4P2_Equipo10 {
+
+    static String [][] matrix = new String [8][8];
+    
+    public static void main(String[] args) {
+        String comando = "";
+        
+        do{
+            
+        }while(comando!="gusbai");
+    }
+    
+}
