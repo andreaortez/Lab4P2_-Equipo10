dabfd664a342fcbe9472580038aaa654b6eec5e4
Andrea Ortez <andreaortez11@unitec.edu>
2023-02-10 22:37:07 -0600

    cambios de peon

--- src/lab4p2_/equipo10/Peon.java
+++ src/lab4p2_/equipo10/Peon.java
@@ -21,45 +21,38 @@ public class Peon extends Pieza {
             Inicio = true;
         }
 
-        for (int i = 1; i < matriz.length - 1; i++) {
-            for (int j = 1; j < matriz[i].length; j++) {
-                if ((i == NovaI) && (j == NovaJ)) {
-                    if (Inicio) {//si no es pieza inicial
-                        if ((matriz[PosI + 1][PosJ + 1]).equals(matriz[NovaI][NovaJ])) {
-                            return true;
-                        } else if ((matriz[PosI - 1][PosJ + 1]).equals(matriz[NovaI][NovaJ])) {
-                            return true;
-                        }
-                    } else {//si es pieza inicial
-                        if (matriz[NovaI][NovaJ] == " ") {
-                            if ((matriz[PosI][PosJ + 2]).equals(matriz[NovaI][NovaJ])) {
-                                return true;
-                            } else if ((matriz[PosI][PosJ + 1]).equals(matriz[NovaI][NovaJ])) {
-                                return true;
-                            }
-                        } else {
-                            if ((matriz[PosI][PosJ + 1]).equals(matriz[NovaI][NovaJ])) {
-                                return true;
-                            }
-                        }
-                    }
-
-                    if (matriz[i][j] == " ") {
-                        if (((j != -1 || j != 10)) && ((j != 0 || j != 9))) {
-                            return true;
-                        } else {
-                            return false;
-                        }
-                    } else {
-                        if ((matriz[PosI + 1][PosJ + 1] != " ") || (matriz[PosI - 1][PosJ + 1] != " ")) {
-                            return true;
-                        }
-                        if (matriz[PosI - 1][PosJ - 1] == (matriz[NovaI][NovaI])) {
-                        }
-                    }
+        if (matriz[NovaI][NovaJ] == " ") {
+            if (Inicio) {//si no es pieza inicial
+                if ((matriz[PosI - 1][PosJ]).equals(matriz[NovaI][NovaJ])) {//peon blanco
+                    return true;
+                } else if ((matriz[PosI + 1][PosJ]).equals(matriz[NovaI][NovaJ])) {//peon negro
+                    return true;
                 }
             }
+
+            if ((matriz[PosI + 2][PosJ]).equals(matriz[NovaI][NovaJ])) {//si es pieza inicial negra
+                return true;
+            } else if ((matriz[PosI + 1][PosJ]).equals(matriz[NovaI][NovaJ])) {
+                return true;
+            } else if ((matriz[PosI - 2][PosJ]).equals(matriz[NovaI][NovaJ])) {//si es pieza inicial blanca
+                return true;
+            } else if ((matriz[PosI - 1][PosJ]).equals(matriz[NovaI][NovaJ])) {
+                return true;
+            }
+        } else {
+            return false;
         }
+
+        if (((NovaI == 0 || NovaI == 9)) && ((NovaJ == 0 || NovaJ == 9))) {
+            return false;
+        } else {
+            if ((matriz[PosI + 1][PosJ + 1] != " ") || (matriz[PosI - 1][PosJ + 1] != " ")) {
+                return true;
+            }
+            if (matriz[PosI - 1][PosJ - 1] == (matriz[NovaI][NovaI])) {
+            }
+        }
+
         return false;
     }
 }
