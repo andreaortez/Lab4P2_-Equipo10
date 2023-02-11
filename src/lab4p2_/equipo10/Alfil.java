af08ae421c098705548520850e66e11d88a8e1e0
Carlosnoe10 <carlosdnoem@gmail.com>
2023-02-10 16:28:16 -0800

    validacion

--- src/lab4p2_/equipo10/Alfil.java
+++ src/lab4p2_/equipo10/Alfil.java
@@ -16,11 +16,10 @@ public class Alfil extends Pieza {
 
     @Override
     public boolean Movimiento(int NovaI, int NovaJ, int PosI, int PosJ, Object[][] matriz) {
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
+        for (int i = 1; i < matriz.length-1; i++) {
+            for (int j = 1; j < matriz[i].length; j++) {
                     if ((j - 1 == -1 || j + 1 == 10)) {
                         if (i - 1 == -1 || i + 1 == 10) {
-                            
                             } else if (matriz[i][j] == " ") {
                                 
                             }
